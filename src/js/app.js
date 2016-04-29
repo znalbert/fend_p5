fc7ea6c560f0eb4af8533ffcfa8bc189eb09ce25
Zoltan <znalbert@gmail.com>
2016-04-29 02:57:51 -0500

    feat: Add Event List and Markers w/ KO.js
    Got the basic funcationality started such that a list of events is
    populating on the map with knockout data-bindings, and got the place
    markers to fall onto the map.

--- src/js/app.js
+++ src/js/app.js
@@ -1,85 +1,113 @@
-var map;
-var coords = {};
-
-function initMap() {
-	map = new google.maps.Map(document.getElementById('map'), {
-		center: coords,
-		zoom: 13,
-		disableDefaultUI: true
-	});
-
-	$('#submit-city').attr("id", "submit-newCity");
-
-	var geocoder = new google.maps.Geocoder();
-
-	document.getElementById('submit-newCity').addEventListener('click', function() {
-	  geocodeNewCity(geocoder, map);
-	});
+var Event = function(data) {
+	this.title = ko.observable(data.title);
+	this.venue = ko.observable(data.venue_name);
 }
 
-function geocodeNewCity(geocoder, resultsMap) {
-	var city = $('#city').val();
-	geocoder.geocode({'address': city}, function(results, status) {
-		if (status === google.maps.GeocoderStatus.OK) {
-			resultsMap.setCenter(results[0].geometry.location);
-		} else {
-			alert('Geocode was not successful for the following reason: ' + status);
+var ViewModel = function() {
+	var self = this;
+
+	this.eventList = ko.observableArray([]);
+
+	var map;
+	var coords = {};
+
+	function initMap() {
+		map = new google.maps.Map(document.getElementById('map'), {
+			center: coords,
+			zoom: 13,
+			disableDefaultUI: true
+		});
+	}
+
+	// function geocodeNewCity(geocoder, resultsMap) {
+	// 	var city = $('#city').val();
+	// 	geocoder.geocode({'address': city}, function(results, status) {
+	// 		if (status === google.maps.GeocoderStatus.OK) {
+	// 			resultsMap.setCenter(results[0].geometry.location);
+	// 		} else {
+	// 			alert('Geocode was not successful for the following reason: ' + status);
+	// 		}
+	// 	});
+	// }
+
+	function getGeoLocation() {
+		if (navigator.geolocation) {
+			navigator.geolocation.getCurrentPosition(function(position) {
+				coords.lat = position.coords.latitude;
+				coords.lng = position.coords.longitude;
+
+				initMap();
+				getEvents();
+			})
 		}
-	});
-}
+	}
 
-function getGeoLocation() {
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function(position) {
-			coords.lat = position.coords.latitude;
-			coords.lng = position.coords.longitude;
+	function getLocationManually() {
+		var geocodeApi = "https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyAZKV6_aryLoI5q40ikKRJ_Qy-32Hg-3ng&address=";
+		var city = $('#city').val();
+		var geocodeUrl = geocodeApi + city;
 
+		self.eventList([]);
+		console.log(city);
+		console.log(geocodeUrl);
+
+		$.getJSON(geocodeUrl, function( data ) {
+			pos = data.results[0].geometry.location;
+			coords.lat = pos.lat;
+			coords.lng = pos.lng;
+			
 			initMap();
 			getEvents();
-		})
+		});
 	}
-}
 
-function getLocationManually() {
-	var geocodeApi = "https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyAZKV6_aryLoI5q40ikKRJ_Qy-32Hg-3ng&address=";
-	var city = $('#city').val();
-	var geocodeUrl = geocodeApi + city;
+	function getEvents() {
+		// API for events is coming from eventful.com
+		var efApi = "http://api.eventful.com/json/events/search?app_key=";
+		var efKey = "VN3TDSXzQdSQK2rD";
+		var efSort = "&date=Today&sort_order=popularity&within=20";
+		var efInclude = "&include=categories,subcategories,popularity";
+		var efLocation = "&location=" + coords.lat + "," + coords.lng;
 
-	console.log(city);
-	console.log(geocodeUrl);
-
-	$.getJSON(geocodeUrl, function( data ) {
-		pos = data.results[0].geometry.location;
-		coords.lat = pos.lat;
-		coords.lng = pos.lng;
+		var efUrl = efApi + efKey + efSort + efInclude + efLocation;
 		
-		initMap();
-		getEvents();
-	});
-}
+		$.ajax({
+			url: efUrl,
+			dataType: "jsonp",
+			success: function( data ) {
+				console.log(data);
 
-function getEvents() {
-	// API for events is coming from eventful.com
-	var efApi = "http://api.eventful.com/json/events/search?app_key=";
-	var efKey = "VN3TDSXzQdSQK2rD";
-	var efSort = "&date=Today&sort_order=popularity&within=25";
-	var efInclude = "&include=categories,subcategories,popularity";
-	var efLocation = "&location=" + coords.lat + "," + coords.lng;
+				data.events.event.forEach(eventViews);
+			}
+		});
+	}
 
-	var efUrl = efApi + efKey + efSort + efInclude + efLocation;
-	
-	$.ajax({
-		url: efUrl,
-		dataType: "jsonp",
-		success: function( data ) {
-			console.log(data);
+	function eventViews(eventObject, index, eventArray) {
+		self.eventList.push( new Event(eventObject));
+
+		console.log(eventObject.venue_name + ", " + eventObject.title);
+		
+		var myLatlng = {
+			lat: Number(eventObject.latitude),
+			lng: Number(eventObject.longitude)
 		}
+
+		console.log(myLatlng);
+		
+		marker = new google.maps.Marker({
+			position: myLatlng,
+			map: map,
+			animation: google.maps.Animation.DROP
+		});
+		
+		marker.setMap(map);
+	}
+
+	getGeoLocation();
+	// $('#getCity').submit(getLocationManually);
+	document.getElementById('submit-city').addEventListener('click', function() {
+		getLocationManually();
 	});
 }
 
-getGeoLocation();
-// $('#getCity').submit(getLocationManually);
-document.getElementById('submit-city').addEventListener('click', function() {
-	getLocationManually();
-});
-
+ko.applyBindings(new ViewModel());
