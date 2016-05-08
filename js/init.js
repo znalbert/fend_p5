215beaca949478d8592878e87fead55b4737facd
Zoltan <znalbert@gmail.com>
2016-05-08 15:46:58 -0500

    style: Move getScript functions to their own files
    The functions inside of <script> tags in my html file weren't being
    minified.  Moved them into their own init.js file. Also, minor edit to
    gulpfile to account for issues inlining my js files.

---
+++ js/init.js
@@ -0,0 +1,1 @@
+$.getScript("https://maps.googleapis.com/maps/api/js?key=AIzaSyCFrRhxyJYPCB01qMXi3zyNTXERl0ArxsQ",function(){$.getScript("js/app.js")}).fail(function(){$("#error").css("display","block"),$("#error").text("Unable to reach Google Maps. Please try again later.")});
